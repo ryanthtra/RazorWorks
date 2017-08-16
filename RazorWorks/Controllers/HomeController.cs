9dfca0384afca09fc693b966c9b47b99f6a5c40f
Ryan Transfiguracion <ryan_transfiguracion@outlook.com>
2017-08-16 00:28:58 -0700

    Initial commit with base MVC files.

---
+++ RazorWorks/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorWorks.Models;
+
+namespace RazorWorks.Controllers
+{
+
+
+
+  public class HomeController : Controller
+  {
+    public ViewResult Index()
+    {
+      Product myProduct = new Product {
+        ProductID = 1,
+        Name = "Kayak",
+        Description = "A boat for one person",
+        Category = "Watersports",
+        Price = 275M
+      };
+
+      return View(myProduct);
+    }
+  }
+}
